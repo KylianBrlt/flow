d4ce5cc9a1410a3b3959c4d0de5225da0068ac81
Kylian BRAULT <kbrault@et.esiea.fr>
2025-03-01 20:15:47 +0100

    dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ui/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
-          <SignInButton mode="modal"/>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <SignInButton mode="modal">
+        <Button> Sign In </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+      <Button variant={"secondary"}> Click me </Button>
     </div>
   );
 }
