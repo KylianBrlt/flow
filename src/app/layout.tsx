97654f81d14db050c2e6cdae754afce14a899f7f
Kylian BRAULT <kbrault@et.esiea.fr>
2025-03-01 22:33:14 +0100

    slidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +30,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -40,21 +39,17 @@ export default function RootLayout({
           >
             <div className="min-h-screen">
               <Navbar />
+
               <main className="py-8">
-                {/* container to center content */}
+                {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:cols-span-3">
-                      sidebar
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
                     </div>
-                    <div className="lg:col-span-9">
-                      {children}
-
-                    </div>
+                    <div className="lg:col-span-9">{children}</div>
                   </div>
-
                 </div>
-
               </main>
             </div>
           </ThemeProvider>
