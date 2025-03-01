8e3a6e85e2379505d04d226a831523022f05b31a
Kylian BRAULT <kbrault@et.esiea.fr>
2025-03-01 20:50:31 +0100

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-        <Button> Sign In </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}> Click me </Button>
+      <h1>Home page content</h1>
     </div>
   );
 }
