ecb459c47d708f4b18d8ca9746fc1686ffefe91e
Kylian BRAULT <kbrault@et.esiea.fr>
2025-03-01 18:55:53 +0100

    Initial Commit / auth

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+          <SignInButton mode="modal"/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+    </div>
+  );
+}
